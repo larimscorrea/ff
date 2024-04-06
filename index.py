da74ec1c033418a7e1ccfa5e6671311089535f51
Larissa Corrêa <larimscorrea@gmail.com>
2024-04-05 21:45:12 -0300

    tem um erro

--- index.py
+++ index.py
@@ -7,9 +7,10 @@ jan.title = "System - Acess Panel"
 jan.geometry("600x300")
 jan.configure(background="white")
 jan.resizable(width=False, height=False)
+jan.attributes("-alpha", 0.9)
 
 logo = PhotoImage(file="icons/icon.png")
-logo = Frame(width=100, height=100)
+#logo = Frame(width=100, height=100)
 
 LeftFrame = Frame(jan, width=200, height=300, bg="MIDNIGHTBLUE", relief="raised")
 LeftFrame.pack(side=LEFT)
@@ -20,7 +21,7 @@ RightFrame.pack(side=RIGHT)
 LogoLabel = Label(LeftFrame, image=logo, bg="MIDNIGHTBLUE")
 LogoLabel.place(x=50, y=100)
 
-UserLabel = Label(RightFrame, text="Username", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
+UserLabel = Label(RightFrame, text="Username: ", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
 UserLabel.place(x=5, y=100)
 
 UserEntry = ttk.Entry(RightFrame, width=30)
@@ -31,5 +32,45 @@ PassLabel.place(x=5, y=150)
 
 PassEntry = ttk.Entry(RightFrame, width=30)
 PassEntry.place(x=150, y=160)
+
+LoginButton = ttk.Button(RightFrame, text="Login", width=30)
+LoginButton.place(x=100, y=200)
+
+def Register():
+    LoginButton.place(x=5000)
+    RegisterButton.place(x=5000)
+
+    NameLabel = Label(RightFrame, text="Name: ", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
+    NameLabel.place(x=5, y=5)
+
+    NameEntry = Entry(RightFrame, width=39)
+    NameEntry.place(x=100, y=16)
+
+    EmailLabel = Label(RightFrame, text="E-mail: ", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
+    EmailLabel.place(x=100, y=66)
+
+    EmailEntry = Entry(RightFrame, width=39)
+    EmailEntry.place(x=100, y=16)
+    
+    Register = ttk.Button(RightFrame, text="Register", width=30)
+    Register.place(x=100, y=225)
+
+    def BackToLogin():
+        NameLabel.place(x=5000)
+        NameEntry.place(x=5000)
+        EmailLabel.place(x=5000)
+        EmailEntry.place(x=5000)
+        Register.place(x=5000)
+        Back.place(x=5000)
+
+        LoginButton.place(x=100)
+        RegisterButton.place(x=125)
+
+    Back = ttk.Button(RightFrame, text="Back", width=20, command=BackToLogin)
+    Back.place(x=125,y=260)
+
+
+RegisterButton = ttk.Button(RightFrame, text="Register", width=20, command=Register)
+RegisterButton.place(x=125, y=260)
 
 jan.mainloop()
